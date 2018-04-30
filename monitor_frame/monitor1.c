d7f6b26f1e8c68b901272765047cc02c9bba979d
wangfanstar <wangfanstar@163.com>
2018-04-30 12:16:15 +0800

    ubuntu test

--- monitor_frame/monitor1.c
+++ monitor_frame/monitor1.c
@@ -12,7 +12,7 @@ void funA()
     int times=0;
     for (i = 0; i < 10000; i++)
     {
-        if (i % 100 == 0) // 未处理溢出
+        if (i % 100 == 0) 
         {
             printf("100 %d times\n",  ++times);
         }
